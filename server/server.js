00833d7141186c13d74ca6627c82e91532643873
Justin Winton <justin.winton@gmail.com>
2018-10-12 10:32:22 -0700

    adds listen to port basic post request and access to database from server

--- server/server.js
+++ server/server.js
@@ -2,12 +2,23 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const { Plan, CodeAnalysis } = require('../database/database');
+
 const app = express();
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const Queries = require('../utils/queries');
-
 //retreive data from GitHub microserver
-
+app.post('/gitHubMicro', (req, res) => {
+  console.log(request.body)
+});
 //retrieve data from VS Code microserver
+
+
+//send analytics data to Interval Updates
+
+
+app.listen(process.env.PORT, () => {
+  console.log(`App listening on port: ${process.env.PORT}`);
+});
