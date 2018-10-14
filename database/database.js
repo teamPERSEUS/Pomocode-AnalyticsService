ce4c4f37ea8177146c8dac5ba66895cc04adb330
Fredrick Lou <fredrick.lou@gmail.com>
2018-10-13 23:00:50 -0700

    Added IDLE time to DB

--- database/database.js
+++ database/database.js
@@ -1,78 +1,78 @@
 const Sequelize = require('sequelize');
 var db = new Sequelize('pomo_analysis', 'root', '', {
-  host: 'localhost',
-  dialect: 'mysql'
+	host: 'localhost',
+	dialect: 'mysql'
 });
 
 // NOTE: Create DB 'pomo_analysis' in mysql before running
 db.authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully')
-  })
-  .catch((err) => {
-    console.error('Unable to connect to the database:', err);
-  });
+	.then(() => {
+		console.log('Connection has been established successfully');
+	})
+	.catch(err => {
+		console.error('Unable to connect to the database:', err);
+	});
 
 /* DB --SCHEMA-- */
 // vsdata table
-const CodeAnalysis = db.define('vscode', {
-  git_id: {
-    type: Sequelize.INTEGER,
-    foreignKey: true
-  },
-  id: {
-    type: Sequelize.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  date: Sequelize.DATEONLY,
-  user: Sequelize.STRING,
-  repo: Sequelize.STRING,
-  issue: Sequelize.STRING,
-  fileName: Sequelize.STRING,
-  intervalNum: Sequelize.INTEGER,
-  state: Sequelize.STRING,
-  time: Sequelize.INTEGER,
-  wordCount: Sequelize.INTEGER,
+const Intervals = db.define('vsCodeIntervals', {
+	git_id: {
+		type: Sequelize.STRING
+	},
+	id: {
+		type: Sequelize.INTEGER,
+		primaryKey: true,
+		autoIncrement: true
+	},
+	date: { type: Sequelize.DATE, defaultValue: Sequelize.NOW },
+	user: Sequelize.STRING,
+	repoUrl: Sequelize.STRING,
+	issue: Sequelize.STRING,
+	fileName: Sequelize.STRING,
+	intervalNum: Sequelize.INTEGER,
+	state: Sequelize.STRING,
+	time: Sequelize.INTEGER,
+	wordCount: Sequelize.INTEGER,
+	idleTime: Sequelize.INTEGER
 });
 
 // plandata table
 const Plan = db.define('plandata', {
-  id: {
-    type: Sequelize.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  git_id: {
-    type: Sequelize.STRING,
-    unique: true,
-  },
-  repo_url: Sequelize.STRING,
-  organization: Sequelize.STRING,
-  username: Sequelize.STRING,
-  reponame: Sequelize.STRING,
-  number: Sequelize.INTEGER,
-  title: Sequelize.STRING,
-  body: Sequelize.TEXT("long"),
-  complete: {
-    type: Sequelize.BOOLEAN,
-    defaultValue: false
-  },
-  planned: {
-    type: Sequelize.BOOLEAN,
-    defaultValue: false
-  },
-  estimate_start_date: Sequelize.DATEONLY,
-  estimate_end_date: Sequelize.DATEONLY,
-  estimate_time: Sequelize.FLOAT,
-  time_remaining: Sequelize.FLOAT,
-  startdate: Sequelize.DATEONLY,
-  enddate: Sequelize.DATEONLY
+	id: {
+		type: Sequelize.INTEGER,
+		primaryKey: true,
+		autoIncrement: true
+	},
+	git_id: {
+		type: Sequelize.STRING,
+		unique: true
+	},
+	repo_url: Sequelize.STRING,
+	organization: Sequelize.STRING,
+	username: Sequelize.STRING,
+	reponame: Sequelize.STRING,
+	number: Sequelize.INTEGER,
+	title: Sequelize.STRING,
+	body: Sequelize.TEXT('long'),
+	complete: {
+		type: Sequelize.BOOLEAN,
+		defaultValue: false
+	},
+	planned: {
+		type: Sequelize.BOOLEAN,
+		defaultValue: false
+	},
+	estimate_start_date: Sequelize.DATEONLY,
+	estimate_end_date: Sequelize.DATEONLY,
+	estimate_time: Sequelize.FLOAT,
+	time_remaining: Sequelize.FLOAT,
+	startdate: Sequelize.DATEONLY,
+	enddate: Sequelize.DATEONLY
 });
 
 // create/connect to tables in the db
 db.sync();
 
 module.exports.db = db;
-module.exports.CodeAnalysis = CodeAnalysis;
+module.exports.Intervals = Intervals;
 module.exports.Plan = Plan;
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const { Plan, CodeAnalysis } = require('../database/database');
+const { Plan, Intervals } = require('../database/database');
 
 const app = express();
 
@@ -11,31 +11,28 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 //retreive data from GitHub microserver
 app.post('/api/plannerMicro', (req, res) => {
-  console.log(req.body)
-  Plan.create(req.body)
-  .catch((err) => {
-    console.log("Error with Planner Micro table:", err);
-    res.status(500).send("Error in obtaining Plan Data");
-    throw (err);
-  });
+	console.log(req.body.data);
+	Plan.create(req.body).catch(err => {
+		console.log('Error with Planner Micro table:', err);
+		res.status(500).send('Error in obtaining Plan Data');
+		throw err;
+	});
 });
 //retrieve data from VS Code microserver
 app.post('/api/vsCodeMicro', (req, res) => {
-  console.log(req.body)
-  CodeAnalysis.bulkCreate(req.body.interval)
-  .catch((err) => {
-    console.log("Error with VSCode Micro table:", err);
-    res.status(500).send("Error in obtaining VSCode Data");
-    throw (err);
-  });
+	console.log(req.body);
+	Intervals.bulkCreate(req.body.data).catch(err => {
+		console.log('Error with VSCode Micro table:', err);
+		res.status(500).send('Error in obtaining VSCode Data');
+		throw err;
+	});
 });
 
 //send analytics data to Interval Updates
 app.get('/api/docs', (req, res) => {
-  console.log()
-})
-
+	console.log();
+});
 
 app.listen(process.env.PORT, () => {
-  console.log(`App listening on port: ${process.env.PORT}`);
+	console.log(`App listening on port: ${process.env.PORT}`);
 });
